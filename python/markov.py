8c1cfdcac39ac9ac1644bbbd350837eb8fcc625f
rab <badger.ea@gmail.com>
2016-02-11 19:19:51 +0000

    Fixed some formatting and syntax issues

--- python/markov.py
+++ python/markov.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python3
 import random
 
-def start_string = "|START|"
+start_string = "|START|"
 
 def wordgenerator(stream):
-"""Yields one word at a time from a stream. Strips newlines and the utf8 byte order mark"""
+    """Yields one word at a time from a stream. Strips newlines and the utf8 byte order mark"""
     yield start_string
     for line in stream.readlines():
         for word in line.split(" "):
             yield word.translate({ord("\n"): None, ord("\ufeff"): None})
 
 def train(stream, word_hash, depth):
-"""Creates a nested "word hash" of words which follow each other, and their count at leaf nodes."""
+    """Creates a nested "word hash" of words which follow each other, and their count at leaf nodes."""
     words = []
     for word in wordgenerator(stream):
         words.append(word)
@@ -23,7 +23,7 @@ def train(stream, word_hash, depth):
             
 
 def update_hash(word_hash, words):
-"""Takes a list of words and increments or creates their leaf node in a word hash"""
+    """Takes a list of words and increments or creates their leaf node in a word hash"""
     sub_hash = word_hash
     for word in words[:-1]:
         if word not in sub_hash.keys():
@@ -35,7 +35,7 @@ def update_hash(word_hash, words):
         sub_hash[words[-1]] = 1
 
 def get_weight(item):
-"""Given a node in a word hash, returns its weight (or the sum of weights of its children)"""
+    """Given a node in a word hash, returns its weight (or the sum of weights of its children)"""
     while True:
         if isinstance(item, int):
             return item
@@ -43,7 +43,7 @@ def get_weight(item):
             return sum([get_weight(val) for key, val in item.items()])
 
 def get_weighted_words(word_hash, words):
-"""Gets a weighted list of word choices directly under the given words in the hash"""
+    """Gets a weighted list of word choices directly under the given words in the hash"""
     sub_hash = word_hash
     for word in words:
         if word in sub_hash.keys():
@@ -53,7 +53,7 @@ def get_weighted_words(word_hash, words):
     return {word: get_weight(val) for word, val in sub_hash.items()}
 
 def choose_weighted(weighted_words):
-"""Chooses an entry at random from a weighted list"""
+    """Chooses an entry at random from a weighted list"""
     total = sum([weight for word, weight in weighted_words.items()])
     i = random.randint(0, total - 1)
     for word, weight in weighted_words.items():
@@ -64,7 +64,7 @@ def choose_weighted(weighted_words):
     
 
 def generate_word(word_hash, words):
-"""Generates a single word given the current list of words from a word hash"""
+    """Generates a single word given the current list of words from a word hash"""
     while len(words) > 0:
         weighted_words = get_weighted_words(word_hash, words)
         if weighted_words:
@@ -75,7 +75,7 @@ def generate_word(word_hash, words):
 
 
 def generate(word_hash, depth, length):
-"""Generates a number of words from a word hash using a given depth"""
+    """Generates a number of words from a word hash using a given depth"""
     words = [start_string]
     for i in range(length):
         words.append(generate_word(word_hash, words[-depth:]))
